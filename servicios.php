06bfd1c1f3cffc55194ccf0776a92268f7c641fe
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2023-03-08 22:30:23 -0300

    Update servicios.php

--- servicios.php
+++ servicios.php
@@ -8,6 +8,8 @@
 	<link rel="stylesheet" href="css/normalize.css" />
 	<link rel="stylesheet" href="iconos.css">
 	<link rel="stylesheet" href="css/libreaccesibilidad.css" />
+	<!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body class="home">
 	<section class="holder">
