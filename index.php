98a1582eb16598802f28444a1d71bb2ac5c51cde
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2023-03-08 22:23:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Libre Accesibilidad</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="css/normalize.css" />	
-	<link rel="stylesheet" href="iconos.css">
 	<!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
 	<link rel="stylesheet" href="css/libreaccesibilidad.css" />
+	
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 	
 </head>
 <body class="home">
@@ -94,7 +99,7 @@
 		<!-- <script src="js/jquery.min.js"></script>
 	<script src="js/bootstrap.bundle.js"></script> -->
 	<?php include('inc/pie.php') ?>
-
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
 </html>
