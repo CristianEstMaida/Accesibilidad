8261cae30fcb200df25b898301a0ae4c3255f1ad
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2023-03-08 22:28:42 -0300

    Update mae.php

--- mae.php
+++ mae.php
@@ -9,6 +9,8 @@
 	<link rel="stylesheet" href="js/fancybox/jquery.fancybox.css">
 	<link rel="stylesheet" href="iconos.css">
 	<link rel="stylesheet" href="css/libreaccesibilidad.css" />
+	<!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body class="home">
 
